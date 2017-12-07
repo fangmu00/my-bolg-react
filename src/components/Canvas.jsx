4bbd91864fa3dc857569c3b244c7093f6b16f3b1
陈玉玲 <chenyuling@chenyulingdeMacBook-Pro.local>
2017-12-08 00:12:53 +0800

    fix:球体动画

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -2,18 +2,6 @@ import React from 'react';
 
 /* global document, requestAnimationFrame */
 
-export const drawStar = (ctx, { items, color }) => {
-  items.forEach((item) => {
-    ctx.fillStyle = item.color;
-    ctx.beginPath();
-    ctx.arc(item.x, item.y, item.l, 0, 2 * Math.PI, true);
-    ctx.closePath();
-    ctx.fill();
-  });
-  ctx.strokeStyle = color;
-  // linkline(ctx, items);
-};
-
 export const linkline = (ctx, items) => {
   if (items.length === 0) {
     return;
@@ -30,6 +18,18 @@ export const linkline = (ctx, items) => {
   ctx.closePath();
   ctx.stroke();
   linkline(ctx, items.slice(1));
+};
+
+export const drawStar = (ctx, { items, color }) => {
+  items.forEach((item) => {
+    ctx.fillStyle = item.color;
+    ctx.beginPath();
+    ctx.arc(item.x, item.y, item.l, 0, 2 * Math.PI, true);
+    ctx.closePath();
+    ctx.fill();
+  });
+  ctx.strokeStyle = color;
+  linkline(ctx, items);
 };
 
 export const setColor = () => {
@@ -53,7 +53,7 @@ class Anim extends React.Component {
     this.el.width = this.clentW;
     this.el.height = this.clentH;
     const ctx = this.el.getContext('2d');
-    this.init(ctx, Math.floor((Math.random() * 0) + 1));
+    this.init(ctx, Math.floor((Math.random() * 3) + 6));
   }
 
   setStar() { // 生成图案对象
@@ -66,16 +66,15 @@ class Anim extends React.Component {
     star.items = []; // 圆球对象
     // star.color = this.setColor();
     star.color = '#f2f2f2';
-    star.itemLen = Math.floor((Math.random() * 3) + 8); // 圆球个数
+    star.itemLen = Math.floor((Math.random() * 2) + 3); // 圆球个数
     for (let i = 0; i < star.itemLen; i += 1) {
       star.items.push({
         x: Math.floor(l + (Math.random() * r)), // 圆心x坐标
         y: Math.floor(t + (Math.random() * b)), // 圆心y坐标
-        l: Math.floor((Math.random() * 10) + 20), // 圆半径
-        // moveAngle: 30,
+        l: Math.floor((Math.random() * 10) + 10), // 圆半径
         moveAngle: Math.floor(360 * Math.random()), // 移动弧度
-        // color: '#f2f2f2'
-        color: setColor(),
+        color: '#f2f2f2',
+        // color: setColor(),
       });
     }
     return star;
@@ -90,22 +89,23 @@ class Anim extends React.Component {
     ctx.clearRect(0, 0, this.clentW, this.clentH);
     starGroup.forEach((item) => {
       drawStar(ctx, item);
-      const i = 5;
+      const speed = 0.5;
       const backItem = {
         items: [],
       };
+
       item.items.forEach((s) => {
         const {
           moveAngle, x, y, l, color,
         } = s;
         const backS = {
-          moveAngle, x, y, l, color,
+          moveAngle: moveAngle % 360, x, y, l, color,
         };
-        const moveY = Math.sin((s.moveAngle / 180) * Math.PI) * i;
+        const moveY = Math.sin((s.moveAngle / 180) * Math.PI) * speed;
         if ((s.moveAngle >= 0 && s.moveAngle < 90) || (s.moveAngle >= 270 && s.moveAngle < 360)) {
-          backS.x = x + i;
+          backS.x = x + speed;
         } else {
-          backS.x = x - i;
+          backS.x = x - speed;
         }
         backS.y = y - moveY;
         if (
@@ -114,25 +114,34 @@ class Anim extends React.Component {
           (backS.y - backS.l) < 0 ||
           (backS.y + backS.l) > this.clentH
         ) {
-          // backS.moveAngle = (backS.moveAngle + Math.PI) % (2 * Math.PI);
-          // if (backS.moveAngle >= 0 && backS.moveAngle < Math.PI * 0.5) {
-          //   backS.moveAngle += ((Math.PI * 0.5) - backS.moveAngle) * 2;
-          // } else if (backS.moveAngle >= Math.PI * 0.5 && backS.moveAngle < Math.PI) {
-          //   backS.moveAngle += ((Math.PI * 0.5) - backS.moveAngle) * 2;
-          // }
           if ((backS.x - backS.l) <= 0) {
             backS.x = backS.l;
+            if (backS.moveAngle >= 90 && backS.moveAngle < 180) {
+              backS.moveAngle -= (backS.moveAngle % 90) * 2;
+            } else {
+              backS.moveAngle += ((90 - (backS.moveAngle % 90)) * 2);
+            }
           } else if ((backS.x + backS.l) >= this.clentW) {
             backS.x = this.clentW - backS.l;
             if (backS.moveAngle >= 0 && backS.moveAngle < 90) {
-              backS.moveAngle += ((90 - backS.moveAngle) * 2) + 180;
+              backS.moveAngle += ((90 - backS.moveAngle) * 2);
             } else {
-              backS.moveAngle = 90 + (backS.moveAngle % 90);
+              backS.moveAngle -= (backS.moveAngle % 90) * 2;
             }
           } else if ((backS.y - backS.l) <= 0) {
             backS.y = backS.l;
+            if (backS.moveAngle > 0 && backS.moveAngle <= 90) {
+              backS.moveAngle += ((90 - backS.moveAngle) * 2) + 180;
+            } else {
+              backS.moveAngle -= ((backS.moveAngle % 90) * 2) - 180;
+            }
           } else if ((backS.y + backS.l) >= this.clentH) {
             backS.y = this.clentH - backS.l;
+            if (backS.moveAngle > 180 && backS.moveAngle <= 270) {
+              backS.moveAngle -= (backS.moveAngle % 90) * 2;
+            } else {
+              backS.moveAngle = 90 - (backS.moveAngle % 90);
+            }
           }
         }
         backItem.items.push(backS);
